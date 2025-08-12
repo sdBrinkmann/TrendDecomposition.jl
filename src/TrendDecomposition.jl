a80c66969f26a6005436904095363a863726a576
Stefan D. Brinkmann <sdbrinkmann@outlook.com>
2025-08-12 13:46:01 +0200

    Moving Averages (MA) and decomposition based on MA
    First basic implementation of moving averages, here the functions
    are denoted as rollingAverages.
    Also simple season component estimation (maSeason) by computing the averages
    for each seasons over the time-series.
    Using MA as trend estimator and the simple season component estimator,
    the classical seasonal decomposition by MA implemented in R
    as decompose{stats} can now be replicated in Julia with maDecomposition.

--- src/TrendDecomposition.jl
+++ src/TrendDecomposition.jl
@@ -6,8 +6,9 @@ using HypothesisTests
 using Statistics
 
 include("HPFilter.jl")
+include("movingAverages.jl")
 greet() = print("Hello World!")
 
-export HP, bHP, bohl_filter
+export HP, bHP, bohl_filter, rollingAverage, maSeason, maDecompose
 
 end # module
